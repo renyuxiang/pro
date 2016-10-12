84025296afc4e758630a0d2c937889cae4de2964
浩汤哥 <1079010282@qq.com>
2016-10-12 19:16:54 +0800

    all'

--- web/src/main/java/com/ryx/server/controller/UserController.java
+++ web/src/main/java/com/ryx/server/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.ryx.server.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.ryx.server.Msg.Result;
+import com.ryx.server.bean.User;
+import com.ryx.server.service.PushService;
 import com.ryx.server.service.UserService;
+import com.ryx.server.util.RedisClientTemplate;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.Random;
+import java.util.UUID;
 
 /**
  * Created by ryx on 2016/10/10.
@@ -22,6 +28,17 @@ public class UserController {
     @Resource
     private UserService userService;
 
+    @Resource(name="userPushService")
+    private PushService userPushService;
+
+    @Resource(name="newsPushService")
+    private PushService newsPushService;
+
+    @Resource(name="clientPushService")
+    private PushService clientPushService;
+
+    @Resource
+    private RedisClientTemplate redisClientTemplate;
     public static String[] args = {
             "18957141081",
             "18957141082",
@@ -35,6 +52,80 @@ public class UserController {
         String result = userService.query(args[random.nextInt(3)]);
         return result;
     }
+
+    @RequestMapping(value = "/setKey")
+    @ResponseBody
+    public String setKey(String name){
+        String result =""+Math.random();
+        String redisResult = redisClientTemplate.set("lff",result);
+        logger.info("set key "+result);
+        return redisResult;
+    }
+
+
+    /**
+     * 用户推送
+     * @param info
+     * @return
+     * @author Administrator
+     * @create 2016-8-10 下午4:22:28
+     */
+    @RequestMapping(value="/user")
+    @ResponseBody
+    public String userPush(User info){
+        Result result = new Result();
+        info.setName("user "+ UUID.randomUUID());
+        try {
+            userPushService.push(info);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return JSON.toJSONString(info);
+    }
+
+    /**
+     * 新闻推送
+     * @param info
+     * @return
+     * @author Administrator
+     * @create 2016-8-10 下午4:22:38
+     */
+    @RequestMapping(value="/news")
+    @ResponseBody
+    public String newsPush(User info){
+        Result result = new Result();
+        info.setName("news "+ UUID.randomUUID());
+        try {
+            newsPushService.push(info);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return JSON.toJSONString(info);
+    }
+    /**
+     * 客户推送
+     * @param info
+     * @return
+     * @author Administrator
+     * @create 2016-8-10 下午4:22:48
+     */
+    @RequestMapping(value="/client")
+    @ResponseBody
+    public String clientPush(User info){
+        Result result = new Result();
+        try {
+            info.setName("client "+ UUID.randomUUID());
+            clientPushService.push(info);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return JSON.toJSONString(info);
+    }
+
+
+
+
+
 
     public static void main(String[] args) {
         System.out.println(random.nextInt(3));
