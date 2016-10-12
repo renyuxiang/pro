2a10e5a7ed9bb99c27baef0348b6cba235ae24ea
浩汤哥 <1079010282@qq.com>
2016-10-12 09:21:38 +0800

    all

--- web/src/main/java/com/ryx/server/Client.java
+++ web/src/main/java/com/ryx/server/Client.java
@@ -1,14 +1,34 @@
 package com.ryx.server;
 
 import com.ryx.server.bean.User;
+import com.ryx.server.service.UserService;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by ryx on 2016/10/10.
  */
 public class Client {
+    public static Logger logger = LoggerFactory.getLogger(Client.class);
     public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("classpath*:application-web.xml");
+        UserService service = context.getBean("userProvider",UserService.class);
         User user = new User();
-        user.setId("123");
-        System.out.println(user.getId());
+        user.setName("ryx");
+        String result = service.insert(user);
+        System.out.println(result);
+        System.out.println(service.query("1"));
+        System.out.println(service.delete("2"));
     }
+
+    @Test
+    public void test(){
+        logger.info("aasss");
+
+    }
+
 }
--- web/src/main/java/com/ryx/server/controller/UserController.java
+++ web/src/main/java/com/ryx/server/controller/UserController.java
@@ -1,7 +1,42 @@
 package com.ryx.server.controller;
 
+import com.ryx.server.service.UserService;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.Random;
+
 /**
  * Created by ryx on 2016/10/10.
  */
+@Controller
 public class UserController {
+    public static Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    public static Random random = new Random();
+    @Resource
+    private UserService userService;
+
+    public static String[] args = {
+            "18957141081",
+            "18957141082",
+            "18957141083",
+    };
+    @RequestMapping(value = "/queryUser")
+    @ResponseBody
+    public String queryUser(String name){
+
+        logger.info("queryUser");
+        String result = userService.query(args[random.nextInt(3)]);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(random.nextInt(3));
+    }
 }
